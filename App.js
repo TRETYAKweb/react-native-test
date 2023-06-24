ecac57d5bbd66cb4edb8f4eb23288c6178d4650d
TretyakWeb <tretyakbro@gmail.com>
2023-06-24 12:22:44 +0300

    feat(App.js): update UI and add input for course goals
    Update the UI in App.js to include an input field and a button for adding course goals. Also, display a list of goals on the screen.

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainet}>
+        <TextInput style={styles.textInput} placeholder="add course goal" />
+        <Button title="Add goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+  },
+  inputContainet: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    placeholder: "black",
+    marginRight: 10,
+    padding: 10,
   },
 });
