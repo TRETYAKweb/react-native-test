6920f64b70abb355eef8d748fa73a8d6974f7adc
TretyakWeb <tretyakbro@gmail.com>
2023-07-01 01:02:26 +0300

    feat(GoalItem): add styles for GoalItem component
    Add the styles for the GoalItem component. The styles define the appearance of the goal item, including padding, border, background color, and text color.

---
+++ src/components/GoalItem/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  goalItem: {
+    padding: 10,
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: "#BDD630",
+    backgroundColor: "#2D414C",
+    margin: 5,
+  },
+  goalText: {
+    color: "#ffffff",
+    fontSize: 14,
+  },
+});
