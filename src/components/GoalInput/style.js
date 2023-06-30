59d1794f0a2a6fa6391c78594918f375ca140a8d
TretyakWeb <tretyakbro@gmail.com>
2023-07-01 01:01:29 +0300

    feat(GoalInput): add styles for GoalInput component
    Add the styles module to the GoalInput component directory. This module contains the styling rules for the GoalInput component, including the input container and text input styles.

---
+++ src/components/GoalInput/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  inputContainet: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
