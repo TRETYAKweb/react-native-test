7556b022cab38c44e6c7e2394ceb6fbfdfea4619
TretyakWeb <tretyakbro@gmail.com>
2023-07-02 00:20:56 +0300

    style(GoalInput): update input container and text input styles
    Update the styles of the GoalInput component to improve its layout and appearance. The input container now has centered content and increased padding. The text input width is set to 100% to occupy the full width of the container. Additionally, the text input has a bottom margin for better spacing. The button container and button styles have been added to create a row layout and provide proper spacing for the buttons.

--- src/components/GoalInput/style.js
+++ src/components/GoalInput/style.js
@@ -3,18 +3,25 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   inputContainet: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 25,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
+    width: "100%",
     padding: 8,
+    marginBottom: 16,
+  },
+  btnContainet: {
+    flexDirection: "row",
+  },
+  button: {
+    width: "30%",
+    marginHorizontal: 6,
   },
 });
