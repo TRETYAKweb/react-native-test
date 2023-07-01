855b708734a3861a967fb8f3520a3568d505a1d4
TretyakWeb <tretyakbro@gmail.com>
2023-07-02 00:20:31 +0300

    feat(GoalInput): add modal functionality and cancel button
    Enhance the GoalInput component by adding modal functionality. The GoalInput component now renders as a modal that can be opened or closed based on the value of the 'isModalOpen' prop. Additionally, a 'Cancel' button has been added to allow users to cancel adding a goal without submitting it.

--- src/components/GoalInput/index.js
+++ src/components/GoalInput/index.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { TextInput, Button, View } from "react-native";
+import { TextInput, Button, View, Modal } from "react-native";
 
 import { styles } from "./style";
 
-export const GoalInput = ({ onAddGoal }) => {
+export const GoalInput = ({ onAddGoal, onCancle, isModalOpen }) => {
   const [enteredGoalText, setEnteredGoalText] = useState("");
 
   const goalInputHandler = (enteredText) => {
@@ -16,14 +16,23 @@ export const GoalInput = ({ onAddGoal }) => {
   };
 
   return (
-    <View style={styles.inputContainet}>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={goalInputHandler}
-        placeholder="add course goal"
-        value={enteredGoalText}
-      />
-      <Button onPress={addGoalHandler} title="Add goal" />
-    </View>
+    <Modal visible={isModalOpen} animationType="slide">
+      <View style={styles.inputContainet}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          placeholder="add course goal"
+          value={enteredGoalText}
+        />
+        <View style={styles.btnContainet}>
+          <View style={styles.button}>
+            <Button onPress={addGoalHandler} title="Add goal" />
+          </View>
+          <View style={styles.button}>
+            <Button onPress={onCancle} title="Cancel" />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 };
