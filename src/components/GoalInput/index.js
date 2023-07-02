38809a6a8ec15fd5726d8c0785ffd59d93d28793
TretyakWeb <tretyakbro@gmail.com>
2023-07-03 00:10:13 +0300

    feat: add image and styling to GoalInput component
    - Imported the `Image` component from `react-native` and `StatusBar` component from `expo-status-bar`.
    - Added an image to the GoalInput component using the `Image` component and provided the image source.
    - Updated the placeholder text to "Add course goal".
    - Adjusted the button colors for cancel and add goal.
    
    These changes enhance the visual appearance of the GoalInput component and provide better user experience.

--- src/components/GoalInput/index.js
+++ src/components/GoalInput/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { TextInput, Button, View, Modal } from "react-native";
+import { TextInput, Button, View, Modal, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import { styles } from "./style";
 
@@ -16,23 +17,34 @@ export const GoalInput = ({ onAddGoal, onCancle, isModalOpen }) => {
   };
 
   return (
-    <Modal visible={isModalOpen} animationType="slide">
-      <View style={styles.inputContainet}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={goalInputHandler}
-          placeholder="add course goal"
-          value={enteredGoalText}
-        />
-        <View style={styles.btnContainet}>
-          <View style={styles.button}>
-            <Button onPress={addGoalHandler} title="Add goal" />
-          </View>
-          <View style={styles.button}>
-            <Button onPress={onCancle} title="Cancel" />
+    <>
+      <Modal visible={isModalOpen} animationType="slide">
+        <View style={styles.inputContainet}>
+          <Image
+            style={styles.image}
+            source={require("../../../assets/images/goal.png")}
+          />
+
+          <TextInput
+            style={styles.textInput}
+            onChangeText={goalInputHandler}
+            placeholder="Add course goal"
+            value={enteredGoalText}
+          />
+          <View style={styles.btnContainet}>
+            <View style={styles.button}>
+              <Button onPress={onCancle} title="Cancel" color={"#E65D5C"} />
+            </View>
+            <View style={styles.button}>
+              <Button
+                onPress={addGoalHandler}
+                title="Add goal"
+                color={"#000000"}
+              />
+            </View>
           </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+    </>
   );
 };
